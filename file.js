1b806d7e0ee187b6ecb76ef4b34895afd2c097fd
Han Sung Bae <hansungbae@Hans-MacBook-Air.local>
2018-06-23 14:22:42 -0400

    Updated JS

--- file.js
+++ file.js
@@ -1,5 +1,35 @@
-$("button").on("click", function(){
-    var athlete = $(this).attr("data-athlete");
+var athleteArr = ["Cristiano Ronaldo", "Lionel Messi", "Michael Jordan", "Kobe Bryant", "LeBron James", "Muhammad Ali", "Roger Federer", "Rafael Nadal", "Tom Brady", "Carson Wentz"];
+
+function addAthleteButtons(){
+    //delete movie buttons prior to adding to avoid repeat buttons
+    $("#athleteGIFs").empty();
+
+    //for loop to iterate over athleteArr variable
+    for(var i = 0; i < athleteArr.length; i++){
+        var append = $("<button type='button' class='btn btn-info'>");
+        append.addClass("athName");
+        append.attr("data-name", athleteArr[i]);
+        append.text(athleteArr[i]);
+        $("#athleteGIFs").append(append);
+    }
+};
+
+//function to put user's Athlete in input box to the webpage as a clickable button to trigger GIFs
+$("#add-athlete").on("click", function(event){
+    //event.preventDefault() to prevent form from trying to submit itself
+    event.preventDefault();
+
+    //grab text from input box
+    var addAthlete = $("#athlete-input").val().trim();
+    athleteArr.push(addAthlete);
+
+    addAthleteButtons();
+});
+
+addAthleteButtons();
+
+$(".btn").on("click", function(){
+    var athlete = $(this).attr("data-name");
     console.log(athlete);
     var queryURL = "https://api.giphy.com/v1/gifs/search?" + "&api_key=YorKFzqJrMLyjl579IkUbSyAgntt27Dj" + "&q=" + athlete;
     console.log(queryURL);
@@ -15,29 +45,31 @@ $.ajax({
             var gifDiv = $("<div class='gif' id='item'>");
             var rating = results[i].rating;
 
-            var p = $("<p>").text("Rating: " + rating);
+            var p = $("<p class='text-white' style='margin-top:45px; font-size: 20px;'>").text("Rating: " + rating.toUpperCase());
 
             var athleteImage = $("<img>");
 
-            athleteImage.attr("src", results[i].images.fixed_height.url);
+            athleteImage.attr("src", results[i].images.fixed_height_still.url);
 
             gifDiv.append(p);
             gifDiv.append(athleteImage);
 
-            $("#athleteGIFs").prepend(gifDiv);
+            $("#GIFs").prepend(gifDiv);
         }
     });
 
-    $(".gif").on("click", function(){
+//
+    $(".img").on("click", function(){
         var state = $(this).attr("data-state");
     
         if(state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
+            athleteImage.attr("src", results[i].images.fixed_height.url);
         } else {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
+            athleteImage.attr("src", results[i].images.fixed_height_still.url);
         }
     });
 });
-
